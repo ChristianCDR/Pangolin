3fbf34973bebcefaa5e9383b9ae5c73c3ae584d6
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-12-03 18:08:42 +0100

    app.js, server.js, formulaires d'inscription et de connexion

---
+++ Pangolin-back/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose= require('mongoose');
+const helmet= require('helmet');
+const app = express();
+
+//connexion db
+const connect= process.env.connexion;
+mongoose.connect(connect, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>console.log('Connexion à MongoDB réussie!'))
+.catch(()=>console.log('Connexion à MongoDB échouée!'));
+
+//en-tetes CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+//Helmet colmate des failles de sécurité connues
+app.use(helmet());
+
+//remplace Body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+app.use('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+module.exports=app;
